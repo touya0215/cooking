c8e961aee9df8ce1d20223d41345c17253afc9c6
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-07-20 15:41:53 +0000

    開発準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,4 @@
 Rails.application.routes.draw do
-  namespace :customer do
-    get 'comments/show'
-  end
-  namespace :customer do
-    get 'likes/index'
-  end
-  namespace :customer do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :admin do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
   root to: "homes#top"
   get 'homes/about'
   devise_for :customers, controllers: {
@@ -34,5 +11,41 @@ Rails.application.routes.draw do
    # passwords:     'admins/passwords',
    # registrations: 'admins/registrations'
   }
+
+  namespace :admin do
+    resources :posts
+  #   get 'posts/new'
+  #   get 'posts/index'
+  #   get 'posts/show'
+  #   get 'posts/edit'
+  # end
+  # namespace :admin do
+    resources :customers
+  #   get 'customers/index'
+  #   get 'customers/show'
+  #   get 'customers/edit'
+  # namespace :admin do
+    resources :comments
+  #   get 'comments/index'
+  # end
+  end
+
+  namespace :customer do
+    resources :comments
+  #   get 'comments/show'
+  # end
+  # namespace :customer do
+    resources :likes
+  #   get 'likes/index'
+  # end
+  # namespace :customer do
+    resources :posts
+  #   get 'posts/new'
+  #   get 'posts/index'
+  #   get 'posts/show'
+  #   get 'posts/edit'
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
