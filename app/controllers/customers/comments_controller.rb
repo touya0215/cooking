c33f1a01732bd42317dbcb7fdb7cc5800d17666a
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-09-21 17:01:49 +0000

    コメント機能

--- app/controllers/customers/comments_controller.rb
+++ app/controllers/customers/comments_controller.rb
@@ -2,18 +2,20 @@ class Customers::CommentsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    comment = current_customer.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @comment = current_customer.comments.new(comment_params)
+    @comment.post_id = post.id
+    @comment.save!
+    # redirect_to post_path(params[:id])
+    redirect_back(fallback_location: root_path) #前の画面に戻る
   end
 
   def show
   end
 
   def destroy
-    comment = Comment.find(params[:id])
-    comment.destroy
+    @comment = Comment.find_by(post_id: params[:post_id])
+    @comment.destroy
+    redirect_back(fallback_location: root_path) #前の画面に戻る
   end
 
 
