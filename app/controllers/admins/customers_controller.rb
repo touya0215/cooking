43632229fef4d2d18e5780f64edb7d3a0373aeb9
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-10-04 15:10:06 +0000

    会員情報編集

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -5,8 +5,29 @@ class Admins::CustomersController < ApplicationController
   end
 
   def show
+    @customers = Customer.find(params[:id])
+    @posts = Post.where(customer_id:params[:id])
   end
 
   def edit
+    @customers = Customer.find(params[:id])
   end
+
+  def update
+    @customers = Customer.find(params[:id])
+    if @customers.update(customer_params)
+      flash[:notice] = "You have updated item successfully."
+      redirect_to admins_customer_path(params[:id])
+    else
+      render :show
+    end
+  end
+
+  private
+
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kama, :first_name_kana, :telephone_number, :is_deleted, :email)
+  end
+
 end
