5b04a5f728f749550b4818b9b7902555060d9de0
touya0215 <110541062+touya0215@users.noreply.github.com>
2023-11-06 12:13:19 +0000

    材料ｘ個数の検索機能実装

--- app/controllers/customers/homes_controller.rb
+++ app/controllers/customers/homes_controller.rb
@@ -8,10 +8,21 @@ class Customers::HomesController < ApplicationController
 
   def search
     search_word = params[:word]
+    search_count_word = params[:count_word]
     search_genre = params[:genre]
     genre_num = Post.genres[search_genre]
     # 料理名の検索機能
-    @posts = Post.where("name LIKE ? and genre = ?", "%#{search_word}%", genre_num)
+    # @posts = Post.where("name LIKE ? or sentence LIKE ? or genre = ?", "%#{search_word}%", "%#{search_count_word}%", genre_num)
+    @posts = Post
+    if params[:word]!=""
+      @posts = @posts.where("name LIKE ?", "%#{search_word}%")
+    end
+    if params[:count_word]!=""
+      @posts = @posts + @posts.where("sentence LIKE ?", "%#{search_count_word}%" )
+    end
+    if params[:genre]!=""
+      @posts = @posts.where("genre = ?", genre_num)
+    end
   end
 
 end
